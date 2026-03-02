6b45958de7f2eaac54f40969ad55054e15a47e37
Amit Maurya <ankitamaurya2908@gmail.com>
2026-03-03 01:37:40 +0530

    Added home page

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header-contents">
+            <h2>Cravings Delivered in Minutes</h2>
+            <p>At JulietCafe, every meal is prepared with passion and delivered with care. Order your favorites and enjoy café-quality dining from the comfort of home.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
